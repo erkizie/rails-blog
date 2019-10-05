fe3a3fcc4bd3d7ba4d7fe919137fa4a12edce16e
Erik <erkizie@gmail.com>
2019-10-05 15:42:28 +0300

    Any new user will have an author role (manage only their posts)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   rolify
+  after_create :assign_default_role
   has_many :articles
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def assign_default_role
+    self.add_role(:author) if self.roles.blank?
+  end
 end
